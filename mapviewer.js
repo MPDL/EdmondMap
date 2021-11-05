1db277c5e04b75330bd19e34b6af9bbcecbf024e
Alina Fleischmann <fleischmann@mpdl.mpg.de>
2021-11-05 12:01:42 +0100

    add project files

---
+++ mapviewer.js
@@ -0,0 +1,15 @@
+function initMap() {
+  console.log("initMap called!");
+    const myLatLng = { lat: -29.563, lng: 131.044 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: myLatLng,
+    });
+  
+    new google.maps.Marker({
+      position: myLatLng,
+      map,
+      title: "Hello World!",
+    });
+  }
+
---
+++ testfetch copy.js
@@ -0,0 +1,121 @@
+fetch("http://localhost/collections.json", { mode: "cors" })
+  .then(function (response) {
+    if (response.status !== 200) {
+      console.log(
+        "Looks like there was a problem. Status Code: " + response.status
+      );
+      return;
+    }
+
+    // Examine the text in the response
+    response.json().then(function (data) {
+      console.log(data.results.length);
+
+      var listContent = [];
+      data.results.forEach((result) => {
+        var additionalInfos = result.additionalInfos;
+        if (additionalInfos) {
+          var coords = [];
+          additionalInfos.forEach((info) => {
+            /*  Object.entries(info).forEach(([key, value]) => {
+                        console.log("key:  "+ key);
+                        console.log("value:  "+ value);
+                    });*/
+            if (info.label == "Geo-coordinates") {
+              coords.push({ coordinate: info.text });
+
+              /**listContent.push({
+                title: result.title,
+                id: result.id,
+                coordinate: info.text,
+              });
+              /**console.log("Coordinates: "+ coordinates)
+
+                        var latLng = coordinates.replace(" ", "").split(",")
+                        var lat = parseFloat(latLng[0]);
+                        var lng = parseFloat(latLng[1]);
+                        var marker = L.marker([lat, lng]).addTo(map);
+
+		                marker.bindPopup("<b>"+ coordinates +"</b>").openPopup();
+                        */
+            }
+          });
+          if (coords.length !== 0) {
+            listContent.push({
+              title: result.title,
+              id: result.id,
+              coordinates: coords,
+            });
+          }
+        }
+      });
+
+      console.log(listContent);
+      makeList(listContent);
+    });
+  })
+  .catch(function (err) {
+    console.log("Fetch Error :-S", err);
+  });
+
+var map = L.map("map").setView([40, 0], 2);
+L.tileLayer(
+  "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=hMlLEQcFQEshE4YQVA2W",
+  {
+    attribution:
+      '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+  }
+).addTo(map);
+
+L.control
+  .scale({
+    metric: true,
+    imperial: false,
+    position: "topright",
+  })
+  .addTo(map);
+
+function makeList(listContent) {
+  var listContainer = document.getElementById("collectionsList");
+  // Make a container element for the list
+
+  /*(listContainer = document.createElement("div")),
+    // Make the list
+    (listElement = document.createElement("ul")),
+    // Add it to the page
+    document.getElementsByTagName("body")[0].appendChild(listContainer);
+  listContainer.appendChild(listElement);*/
+
+  listContent.forEach((element) => {
+    // create an item for each one
+    listItem = document.createElement("li");
+
+    // Add the item text
+    listItem.innerHTML = element.coordinates;
+    listItem.innerHTML =
+      '<a class = "animation" href="' +
+      "https://edmond.mpdl.mpg.de/imeji/collection/nd_EdbS5XwdDxT3D" +
+      '" target="_blank">' +
+      element.title +
+      "</a>";
+    listItem.onmouseover = function () {
+      console.log(element.title);
+      console.log(element.coordinates);
+      element.coordinates.forEach((value) => {
+        var latLng = value.coordinate.replace(" ", "").split(",");
+        var lat = parseFloat(latLng[0]);
+        var lng = parseFloat(latLng[1]);
+        var marker = L.marker([lat, lng]);
+        marker.addTo(map);
+        marker
+          .bindPopup(
+            "<b>" + element.title + "</b><br>[" + value.coordinate + "]"
+          )
+          .openPopup();
+      });
+    };
+
+    // Add listItem to the listElement
+    listContainer.appendChild(listItem);
+  });
+}
